0852d002c4c59c1082667776d2eb96d0f6d47512
James Baker <j.baker@outlook.com>
2024-12-29 10:59:42 +0000

    Add few more programs

--- home.nix
+++ home.nix
@@ -1,13 +1,22 @@
-{ ... }: {
-    programs.fzf.enable = true;
+{ pkgs, ... }:
+{
+  programs.fzf.enable = true;
 
-    programs.zsh = {
-        enable = true;
-        
-        oh-my-zsh = {
-            enable = true;
-            plugins = [ "git" ];
-            theme = "agnoster";
-        };
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "agnoster";
     };
+  };
+
+  programs.git.enable = true;
+
+  programs.tmux.enable = true;
+
+  environment.homePackages = with pkgs; [
+    jq
+  ];
 }

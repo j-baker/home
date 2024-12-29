a59deab617f0efa5476192c01818655a3471dfd5
James Baker <j.baker@outlook.com>
2024-12-29 10:43:42 +0000

    checkpoint

--- flake.nix
+++ flake.nix
@@ -1,9 +1,21 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   inputs.home-manager.url = "github:nix-community/home-manager/release-24.11";
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeModules.default = {...}: {};
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
-  }
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs =
+    { nixpkgs, home-manager, flake-utils, ... }: let
+      module = ./home.nix;
+    in {
+      homeModules.default = module;
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
+    } // flake-utils.lib.eachDefaultSystem (system: let 
+      pkgs = (import nixpkgs { inherit system; });
+    in {
+      homeConfigurations.test = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ module ];
+      };
+    });
 }
